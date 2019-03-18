fece1132ab4b17f11e750c0df1ae227ee94694b4
Sandra <otterstensandra@gmail.com>
2019-03-18 10:55:33 +0100

    Username

--- routes/auth.js
+++ routes/auth.js
@@ -24,20 +24,20 @@ module.exports.post = async (req, res) => {
         res.status(402).send('YOU SHALL NOT PASS')
     }
     // if missmatch, return 402
-    
+
 }
 
 module.exports.isAdmin = async (authtoken) => {
-    
+
     // Decode JWT with process.env.SECRET
-    let token = await jwt.verify(authtoken.substring(7), process.env.SECRET)
-    
+    let token = await jwt.verify(authtoken.substring(7), process.env.PASSWORD)
+
     // Get user from db with decoded token > uid
     let user = await User.findOne({uid: token.uid})
 
     // Get user role and return true / false
     if (user.role === 'admin') {
-        return true; 
+        return true;
     } else {
         return false;
     }
@@ -46,12 +46,12 @@ module.exports.isAdmin = async (authtoken) => {
 module.exports.verifyToken = async (token) => {
 
      console.log(token)
-    
+
     try {
         // Verify JWT with process.env.SECRET, return token
-        await jwt.verify(token.substring(7), process.env.SECRET)
-        return true; 
-        
+        await jwt.verify(token.substring(7), process.env.PASSWORD)
+        return true;
+
     } catch(err){
         // if error = not valid token, return 'not valid token.'
         console.error(err);
